70e3d1626f4ed7bdaa589d3a0208ab0efeab3036
Digilad <digilad@gmail.com>
2018-05-08 16:43:44 +0300

    First commit

---
+++ Routing/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Routing.Models;
+
+namespace Routing.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Result", new Result {
+                Controller = nameof(CustomerController),
+                Action = nameof(Index)
+            });
+        }
+
+        public IActionResult List(string id)
+        {
+            Result r = new Result
+            {
+                Controller = nameof(CustomerController),
+                Action = nameof(List)
+            };
+            r.Data["Id"] = id ?? "<no data>";
+            r.Data["CatchAll"] = RouteData.Values["catchall"];
+            return View("Result", r);
+        }
+    }
+}
---
+++ Routing/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Routing.Models;
+
+namespace Routing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Result", new Result {
+                Controller = nameof(HomeController),
+                Action = nameof(Index)
+            });
+        }
+
+        public IActionResult CustomVariable(string id)
+        {
+            Result r = new Result
+            {
+                Controller = nameof(HomeController),
+                Action = nameof(CustomVariable)
+            };
+            //r.Data["Id"] = RouteData.Values["id"];
+            r.Data["Id"] = id;
+            return View("Result", r);
+        }
+    }
+}
